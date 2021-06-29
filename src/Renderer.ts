8a17b359305253a42170a100cfa105df6594417f
genbs <gennaro.bs@gmail.com>
2021-06-29 16:58:12 +0200

    add zip

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -1,41 +1,83 @@
-import { DrawerCanvas } from './DrawerCanvas'
-import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
-import { bBrowser, bNode } from './utils'
+import { createFFmpeg, fetchFile, FFmpeg } from '@ffmpeg/ffmpeg'
 import { Canvas, JpegConfig, PdfConfig, PngConfig } from 'canvas'
+import * as JSZip from 'jszip'
+
+import { DrawerCanvas } from './DrawerCanvas'
+import { bBrowser, bNode } from './utils'
 
 type BoB = Blob | Buffer
 type OoQ = PngConfig | JpegConfig | PdfConfig | number
 
 class Renderer {
+	private ffmpeg: FFmpeg | undefined
 	private drawer: DrawerCanvas
+	private ffmpegCorePath: string | undefined
 
-	constructor(drawer: DrawerCanvas) {
+	constructor(drawer: DrawerCanvas, ffmpegCorePath?: string) {
 		this.drawer = drawer
+		this.ffmpegCorePath = ffmpegCorePath
 	}
 
-	public async render(type: 'video/webm' | 'video/mp4' = 'video/mp4', quality: number = 1): Promise<Uint8Array> {
-		// const ffmpeg = createFFmpeg({ log: true, corePath: 'https://unpkg.com/@ffmpeg/core@0.10.0/dist/ffmpeg-core.js' })
-		const ffmpeg = createFFmpeg({
-			log: false,
-			logger: e => {
-				console.log('log', e)
-			},
-			progress: e => {
-				console.log('progress', e)
-			},
-		})
-		await ffmpeg.load()
+	public async zip(
+		imagesType: 'image/jpeg' | 'image/png' = 'image/png',
+		quality: number = 1,
+		framesForChunk = 600
+	): Promise<Array<Uint8Array>> {
+		const zip = new JSZip()
+		const frames = this.drawer.timeline.getFramesCount()
+		const chunks = Math.ceil(frames / framesForChunk)
+
+		const extension = imagesType === 'image/jpeg' ? '.jpg' : '.png'
+
+		const zipParts: Array<Uint8Array> = []
+
+		for (let chunk = 0; chunk < chunks; chunk++) {
+			for (let frameIndex = 0; frameIndex < framesForChunk; frameIndex++) {
+				const frame = frameIndex + chunk * framesForChunk
+				if (frame < frames) {
+					const frameName = frame.toString().padStart(5, '0') + extension
+
+					const blob = await this.frame(frame, imagesType, quality)
+					const buffer = (await (bNode ? blob : (blob as Blob).arrayBuffer())) as Buffer | ArrayBuffer
+					zip.file(frameName, new Uint8Array(buffer, 0, buffer.byteLength))
+				}
+			}
+			zipParts.push(await zip.generateAsync({ type: 'uint8array' }))
+		}
+
+		return zipParts
+	}
+
+	public async render(
+		type: 'video/webm' | 'video/mp4' = 'video/mp4',
+		quality: number = 1,
+		// TODO: move into contructor
+		ffmpegLogger?: (logParams: { type: string; message: string }) => any,
+		ffmpegProgress?: (progressParams: { ratio: number }) => any
+	): Promise<Uint8Array> {
+		if (!this.ffmpeg) {
+			const ffmpegOptions: any = {
+				log: false,
+			}
+
+			if (this.ffmpegCorePath) ffmpegOptions.corePath = this.ffmpegCorePath
+			if (ffmpegLogger) ffmpegOptions.logger = ffmpegLogger
+			if (ffmpegProgress) ffmpegOptions.progress = ffmpegProgress
+
+			this.ffmpeg = createFFmpeg(ffmpegOptions)
+
+			await this.ffmpeg.load()
+		}
 
 		const frames = this.drawer.timeline.getFramesCount()
 		const framerate = this.drawer.timeline.getFramerate().toString()
 
-		console.log({ frames, framerate })
 		for (let frame = 0; frame < frames; frame++) {
 			const blob = await this.frame(frame, 'image/jpeg', quality)
 			const buffer = (await (bNode ? blob : (blob as Blob).arrayBuffer())) as Buffer | ArrayBuffer
 
 			const frameName = frame.toString().padStart(5, '0') + '.jpg'
-			ffmpeg.FS('writeFile', frameName, new Uint8Array(buffer, 0, buffer.byteLength))
+			this.ffmpeg.FS('writeFile', frameName, new Uint8Array(buffer, 0, buffer.byteLength))
 		}
 
 		const args = ['-r', framerate, '-i', '%05d.jpg']
@@ -48,9 +90,10 @@ class Renderer {
 		}
 
 		args.push('-pix_fmt', 'yuv420p', 'out.mp4')
-		console.log('args1', { args })
-		await ffmpeg.run(...args)
-		const result = await ffmpeg.FS('readFile', 'out.mp4')
+
+		await this.ffmpeg.run(...args)
+		const result = await this.ffmpeg.FS('readFile', 'out.mp4')
+
 		return result
 	}
 
